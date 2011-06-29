f7981e8bf1e5854d49cb9ba133a2183713edf3cf
Simon Jouet <simon.jouet@gmail.com>
2011-06-29 22:45:09 +0100

    .gitignore is now working

--- tdp4/trunk/software/LEADSoft/build/Debug/GNU-Linux-x86/LinkedList.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux-x86/LinkedList.o: LinkedList.c LinkedList.h
-
-LinkedList.h:
--- tdp4/trunk/software/LEADSoft/build/Debug/GNU-Linux-x86/main.o.d
+++
@@ -1,7 +0,0 @@
-build/Debug/GNU-Linux-x86/main.o: main.c LinkedList.h config.h widget.h
-
-LinkedList.h:
-
-config.h:
-
-widget.h:
